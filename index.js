c0e8b599ddc13378d95354daed162e3cee68cb9f
HasieEst <ayrtonm0@gmail.com>
2022-12-13 14:04:41 +0200

    Add files to JSON and clear JSON File entirely

--- index.js
+++ index.js
@@ -44,14 +44,56 @@ app.get('/delete-task/:taskId', (req, res) => {
                 }
             })
             fs.writeFile('./tasks.json', JSON.stringify(tasks, null, 2), 'UTF-8', (err) => {
-                console.log('Error: ', err)
+                if(err != null) console.log('Error: ', err)
             })
             res.redirect('/')
         } catch (err) {
-            console.log('Error', err)
+            if (err != null) console.log('Error', err)
         }
     })
 })
 
+// process form post method data
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+
+app.post('/', (req, res) => {
+    fs.readFile('./tasks.json', 'UTF-8', (err, jsonString) => {
+        if (err) {
+            console.log('Error:', err)
+            return
+        }
+        try {
+            const tasks = JSON.parse(jsonString)
+            let index
+            if(tasks.length ===0){
+                index = 0
+            }else{
+                index = tasks[tasks.length-1].id +1
+            }
+            const newTask = {
+                'id': index,
+                'task': req.body.task
+            }
+            console.log(newTask)
+            tasks.push(newTask)
+            fs.writeFile('./tasks.json', JSON.stringify(tasks, null, 2), 'UTF-8', (err) => {
+                console.log('Error: ', err)
+            })
+            res.redirect('/')
+        } catch (err) {
+            if (err != null) console.log('Error', err)
+        }
+    })
+})
+
+app.get('/clearAll/', (req, res) => {
+    fs.writeFile('./tasks.json', JSON.stringify([]), (err)=>{
+        if(err != null) console.log('Error', err)
+    })
+    res.redirect('/')
+})
 
 app.listen(3001)
