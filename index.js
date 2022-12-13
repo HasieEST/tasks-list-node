36060ff94739d9a3ac7604544447abb5167ccd15
HasieEst <ayrtonm0@gmail.com>
2022-12-13 11:34:59 +0200

    Add Static files - CSS and JS for styling

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
+// Initialize rendering engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// Static file usage - CSS, JS, Images
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-    res.render('index.ejs')})
+    res.render('index')})
 app.listen(3001)
